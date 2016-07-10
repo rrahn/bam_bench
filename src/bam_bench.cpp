45663ae53650da4d1a0a2780f2be02ab6890a16c
rrahn <rene.rahn@fu-berlin.de>
2016-07-10 16:33:33 +0200

    Adapt the test to simple read/write operations.

--- src/bam_bench.cpp
+++ src/bam_bench.cpp
@@ -15,44 +15,51 @@ using namespace seqan;
 
 int main(int argc, char* argv[])
 {
-    BamFileIn bamFile;
-    if (!open(bamFile, argv[1]))
+    for (unsigned i = 0; i < 100; ++i)
     {
-        std::cerr << "Could not open: " << argv[1] << std::endl;
-        return EXIT_FAILURE;
+        BamFileIn bamFile;
+        if (!open(bamFile, argv[1]))
+        {
+            std::cerr << "Could not open: " << argv[1] << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        BamFileOut bamFileOut(context(bamFile));
+        if (!open(bamFileOut, argv[2]))
+        {
+            std::cerr << "Could not open: " << argv[2] << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        BamHeader bamHeader;
+        readHeader(bamHeader, bamFile);
+        writeHeader(bamFileOut, bamHeader);
+
+        seqan::BamAlignmentRecord record;
+        while (!atEnd(bamFile))
+        {
+            readRecord(record, bamFile);
+            writeRecord(bamFileOut, record);
+        }
     }
 
-    BamHeader bamHeader;
-    readHeader(bamHeader, bamFile);
-
-    using TPosition = typename Position<BamFileIn>::Type;
-    seqan::String<seqan::Pair<TPosition, int> > recs;
-
-    seqan::BamAlignmentRecord record;
-    while (!atEnd(bamFile))
-    {
-        TPosition ofs = position(bamFile);
-        readRecord(record, bamFile);
-        appendValue(recs, seqan::Pair<TPosition, int>(ofs, record.beginPos));
-    }
-
-    for (size_t j = 0; j < length(recs); ++j)
-    {
-        setPosition(bamFile, recs[j].i1);
-        readRecord(record, bamFile);
-//        SEQAN_ASSERT_EQ(record.beginPos, recs[j].i2);
-    }
-
-    std::mt19937 rng(/*seed=*/1);
-    std::uniform_int_distribution<long> pdf(0, length(recs) - 1);
-
-    for (size_t i = 0; i < 100000; ++i)
-    {
-        long j = pdf(rng);
-        setPosition(bamFile, recs[j].i1);
-        readRecord(record, bamFile);
-//        SEQAN_ASSERT_EQ(record.beginPos, recs[j].i2);
-    }
+//    for (size_t j = 0; j < length(recs); ++j)
+//    {
+//        setPosition(bamFile, recs[j].i1);
+//        readRecord(record, bamFile);
+////        SEQAN_ASSERT_EQ(record.beginPos, recs[j].i2);
+//    }
+//
+//    std::mt19937 rng(/*seed=*/1);
+//    std::uniform_int_distribution<long> pdf(0, length(recs) - 1);
+//
+//    for (size_t i = 0; i < 100000; ++i)
+//    {
+//        long j = pdf(rng);
+//        setPosition(bamFile, recs[j].i1);
+//        readRecord(record, bamFile);
+////        SEQAN_ASSERT_EQ(record.beginPos, recs[j].i2);
+//    }
 
     return EXIT_SUCCESS;
 }
